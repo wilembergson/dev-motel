e1134310c55f28ae2fb624125d2dc9a0990a324d
wilembergson <wilembergson@yahoo.com>
2024-02-26 21:28:26 -0300

    feat: create account email html structure

--- backend/src/main/java/com/example/backend/service/impl/EmailServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/impl/EmailServiceImpl.java
@@ -1,10 +1,16 @@
 package com.example.backend.service.impl;
 
+import com.example.backend.email.EmailReader;
 import com.example.backend.model.dto.EmailDTO;
 import com.example.backend.service.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -24,12 +30,40 @@ public class EmailServiceImpl implements EmailService {
         mailSender.send(message);
     }
 
-    public void sendCreateAccountEmail(String email){
-        var message = new SimpleMailMessage();
-        message.setFrom("noreply@email.com");
-        message.setTo(email);
-        message.setSubject("Conta DevMotel criada.");
-        message.setText("Sua conta no site da DevMotel for criada com sucesso. Faça o login.");
-        mailSender.send(message);
+    public void sendCreateAccountEmail(String email) {
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom("noreply@email.com");
+            helper.setTo(email);
+            helper.setSubject("Conta DevMotel criada.");
+
+            // Leitura do arquivo HTML
+            String htmlBody = EmailReader.readEmailTemplate("templates/email_created.html");
+
+            helper.setText(htmlBody, true); // true indicates HTML content
+            mailSender.send(message);
+        } catch (MessagingException | IOException e) {
+            // Handle exception
+            e.printStackTrace();
+        }
     }
+
+    /*public void sendCreateAccountEmail(String email) {
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom("noreply@email.com");
+            helper.setTo(email);
+            helper.setSubject("Conta DevMotel criada.");
+
+            String htmlBody = "<h1>Sua conta no site da DevMotel foi criada com sucesso.</h1>"
+                    + "<p>Faça o login para acessar sua conta.</p>";
+            helper.setText(htmlBody, true); // true indicates HTML content
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            // Handle exception
+            e.printStackTrace();
+        }
+    }*/
 }
